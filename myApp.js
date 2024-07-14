5c33d5204a56ece74b763dd4e58926b26cc138bc
angel <angeldrummer2000@gmail.com>
2024-07-14 15:41:44 -0700

    server started, static assets

--- myApp.js
+++ myApp.js
@@ -1,13 +1,13 @@
 let express = require('express');
 let app = express();
 
+absolutePath = __dirname + "/views/index.html"
 
+app.use('public', express.static(__dirname + '/public'));
 
-
-
-
-
-
+app.get('/', (req,res) => {
+    res.sendFile(absolutePath);
+});
 
 
 
