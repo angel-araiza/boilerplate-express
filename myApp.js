0a1cc255d79dc158c684294dfe2a1148a0df26da
angel <angeldrummer2000@gmail.com>
2024-07-15 06:00:56 -0700

    created .env file & served JSON data to '/json' url

--- myApp.js
+++ myApp.js
@@ -1,12 +1,17 @@
+require('dotenv').config()
 let express = require('express');
 let app = express();
 
 absolutePath = __dirname + "/views/index.html"
 
-app.use('public', express.static(__dirname + '/public'));
+app.use('/public', express.static(__dirname + '/public'));
 
-app.get('/', (req,res) => {
-    res.sendFile(absolutePath);
+app.get('/json', (req,res) => {
+    let message = "Hello json";
+    if (process.env.MESSAGE_STYLE === 'uppercase'){
+        message = message.toUpperCase();
+    }
+    res.json({"message": message});
 });
 
 
